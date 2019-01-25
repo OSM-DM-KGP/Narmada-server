028c5cf22e8ff9f325a1a4cb5f6b8c88b2370c34
Kaustubh Hiware <hiwarekaustubh@gmail.com>
2019-01-25 14:25:10 +0530

    port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -15,4 +16,5 @@ def empty():
 # add routes for nodejs backend via here as well
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port, debug=True)
