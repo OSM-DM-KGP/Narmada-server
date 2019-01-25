2b9816575e9d45ba7cfbd1f4c73b270ef2884259
Kaustubh Hiware <hiwarekaustubh@gmail.com>
2019-01-25 14:20:33 +0530

    hello route

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ app = Flask(__name__)
 # get_resource_class.py's version as an api
 
 @app.route('/')
-def hello():
+def base():
 	with open('index.html', 'r') as f:
 		txt = f.readlines()
 	return ''.join(txt)
 
+@app.route('/hello')
+def empty():
+	return "Hello World!"
 # add routes for nodejs backend via here as well
 
 if __name__ == '__main__':
