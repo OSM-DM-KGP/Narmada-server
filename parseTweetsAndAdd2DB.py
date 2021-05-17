8a2d20d185607338011e4c4b810e729b6be121cf
rakaar <rka87338@gmail.com>
2021-05-17 19:23:26 +0000

    Added script to add tweets in mongodb

--- parseTweetsAndAdd2DB.py
+++ parseTweetsAndAdd2DB.py
@@ -5,7 +5,7 @@ import json
 from pymongo import MongoClient
 client = MongoClient('mongodb://127.0.0.1:27017')
 db = client["narmada"]
-tweets_collection = db['tweets']
+tweet = db['tweets']
 
 
 def parseTweet(text_org):
@@ -37,31 +37,64 @@ def parseTweet(text_org):
     for ptr in places_to_remove:
         del places[ptr]
 
+
     # print("\n\n\nText: "+str(text_org))
     print("\nLocation: ",places)
     print("\nResources: "+resource_text)
 
+    # make places in db format
+    locations = {}
+    for place in places:
+        name_of_place = place[0]
+        arr_of_co_ords = place[1]
+        if len(arr_of_co_ords) == 1:
+            locations[name_of_place] = {
+                'lat': arr_of_co_ords[0][0],
+                'long': arr_of_co_ords[0][1]
+            }
+        else:
+            for i in range(len(arr_of_co_ords)):
+                locations[name_of_place + '_' + str(i+1)] = {
+                    'lat': arr_of_co_ords[i][0],
+                    'long': arr_of_co_ords[i][1],
+                }
+
+
+    print('locations in DB ', locations)
     if "need" in text or "require" in text:
+        classification_type = 'Need'
         print("\nType: Need")
     elif "availab" in text or len(resource_text) != 0:
+        classification_type = 'Availability'
         print("\nType: Availability")
     else:
+        classification_type = 'Other'
         print("\nType: Other")
     
     # inserting in mongodb
-    db.tweets_collection.insert_one({
-        'text': text_org,
-        # need to add resource
-        # need to add type
-        # need to add locations
-    })
+    if classification_type != 'Other':
+        try:
+            db.tweets.insert_one({
+                'text': text_org,
+                'Classification': classification_type,
+                'ResourceWords': resource_text.split(" "),
+                "Locations": locations
+            })
+            print('successfully added in DB')
+        except Exception as e:
+            print('Error in adding to DB is ',e)
+
+# For testing uncomment below
+# parseTweet('I need oxygen and bed in goa and hyderabad')
 
 with open('2021-05-10.jsonl') as f:
     json_lines = f.readlines()
-    for json_line in json_lines:
-        parsed_json_line = json.loads(json_line)
-        tweet = parsed_json_line['tweet']
-        print('============> parsing currently ', tweet)
-        parseTweet(tweet)
-        break
+    try:
+        for json_line in json_lines:
+            parsed_json_line = json.loads(json_line)
+            tweet = parsed_json_line['tweet']
+            print('============> parsing currently ', tweet)
+            parseTweet(tweet)
+    except Exception as e:
+        print('Failed for some thing')     
 
